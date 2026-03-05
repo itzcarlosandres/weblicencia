8565293662d9e2303203333c86a095bd18ff3518
PichoX <101039952+PichoX@users.noreply.github.com>
2026-03-05 17:02:36 -0500

    fuente

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { ClientLayout } from '@/components/client-layout'
 const montserrat = Montserrat({
   subsets: ['latin'],
   variable: '--font-sans',
-  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  weight: ['300', '400', '500', '600', '700'],
 })
 
 export const metadata: Metadata = {
